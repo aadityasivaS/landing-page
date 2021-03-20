9ab86efca383918d0b427cff6b1aa5ad7bb6fb19
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-03-20 11:46:10 +0530

    ready

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function Home() {
   return (
@@ -10,17 +10,19 @@ export default function Home() {
       </Head>
       <main className="flex h-screen">
         <div className="m-auto">
-          <Player
-            autoplay
-            loop
-            src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
-            style={{ height: "300px", width: "300px" }}
-          >
-            <Controls
-              visible={true}
-              buttons={["play", "repeat", "frame", "debug"]}
-            />
-          </Player>
+          <div className="flex space-x-6">
+            <Player
+              autoplay
+              loop
+              src="https://assets4.lottiefiles.com/packages/lf20_atjj0tgb.json"
+              style={{ height: "400px", width: "400px" }}
+            ></Player>
+            <div className="h-auto m-auto space-y-3">
+              <h1 className="text-5xl font-semibold">We are launching soon!</h1>
+              <h4 className="text-2xl">© {new Date().getFullYear()} Factorial</h4>
+
+            </div>
+          </div>
         </div>
       </main>
     </div>
